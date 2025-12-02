34ee1a47f399e52b3b19ddbd898b0ddf8af5a896
yyonee-com <yuni.eka.2203126@students.um.ac.id>
2025-12-02 20:57:35 +0700

    update-profil

--- resources/views/dashboard-siswa.blade.php
+++ resources/views/dashboard-siswa.blade.php
@@ -126,7 +126,7 @@
             <div class="bg-[#00537A] p-4 rounded-[16px] shadow-xl border border-[#A8E8F9]/10 sticky top-4 h-full">
                 <h5 class="text-lg font-bold text-white mb-4 border-b border-[#A8E8F9]/20 pb-2">Profil Siswa</h5>
                         {{-- Tombol Edit di pojok kanan atas --}}
-                <a href="{{ route('siswa.edit', $siswa->id) }}"class="absolute top-2 right-4 bg-gold hover:bg-yellow-500 text-[#013C58] font-bold p-2 rounded-full shadow-lg shadow-gold/10 transition-all transform hover:-translate-y-1 hover:shadow-gold/30" title="Edit Profil">
+                <a href="{{ route('siswa.edit-profil', $siswa->id) }}"class="absolute top-2 right-4 bg-gold hover:bg-yellow-500 text-[#013C58] font-bold p-2 rounded-full shadow-lg shadow-gold/10 transition-all transform hover:-translate-y-1 hover:shadow-gold/30" title="Edit Profil">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                     </svg>
