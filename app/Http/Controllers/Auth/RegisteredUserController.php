a002b8b936fe63a235ad357764c58b4738d47377
emillieanastasia <anastasia.emillie.2203126@students.um.ac.id>
2025-12-03 13:37:19 +0700

    update register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\siswa;
+use App\Models\Siswa;
 use App\providers\AppServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
